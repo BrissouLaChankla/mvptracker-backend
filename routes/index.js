043f827d3cc5e96b5cd044b43f468f5995c09e04
Brice Eliasse <brice.eliasse@gmail.com>
2023-10-31 18:26:01 +0100

    update the only date in bdd rather than create new lastUpdate

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.post('/storeGames', function (req, res) {
   Game.findOne({ op_link }).then(game => {
     if (game) {
       res.json({ result: false, message: "Game déjà existante" });
-     
+
     } else {
       const newGame = new Game({
         date,
@@ -23,13 +23,12 @@ router.post('/storeGames', function (req, res) {
         res.json({ result: true, message: "Nouvelle game ajoutée en BDD !" });
       })
 
-    const newUpdate = new Update({
-      lastUpdate: new Date()
-    });
-    
-    newUpdate.save().then(() => {
-      console.log("lets go new update")
-    });
+
+      Update.findOne({}).then(data => {
+        Update.updateOne({ _id: data._id }, { lastUpdate: new Date() }).then(() => {
+          console.log("lastUpdate mis à jour")
+        });
+      })
     }
 
   })
